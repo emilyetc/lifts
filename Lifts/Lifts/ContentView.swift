801f9a5e07f2385b946ca48a8097cd5d4c67d7a7
Atulya Lohani <al878@cornell.edu>
2023-06-09 10:37:27 -0500

    Initialized xcode project

---
+++ Lifts/Lifts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lifts
+//
+//  Created by Atulya Lohani on 6/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lifts/Lifts/LiftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftsApp.swift
+//  Lifts
+//
+//  Created by Atulya Lohani on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LiftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
