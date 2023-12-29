05a17a52c228639758583444dae508cacf54a1a9
Emily Wang & Atulya Lohani <alohani@Atulyas-MacBook-Air.local>
2023-12-28 18:45:13 -0600

    created project

---
+++ lifts/lifts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  lifts
+//
+//  Created by Emily Wang & Atulya Lohani on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lifts/lifts/liftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  liftsApp.swift
+//  lifts
+//
+//  Created by Emily Wang & Atulya Lohani on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct liftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
